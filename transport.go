2adba90967ef85b9482e2019627353f0972dd04b
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2022-04-06 21:55:55 +0530

    interface and error implementation for transport

---
+++ transport.go
@@ -0,0 +1,30 @@
+package chanpubsub
+
+import "fmt"
+
+type Transport interface {
+	// Receive gets a channel on which to receive messages with the specified name
+	Receive(name string) <-chan []byte
+	// Send gets a channel on which messages with the specified name may be sent.
+	Send(name string) chan<- []byte
+	// ErrChan gets a channel through which errors are sent
+	ErrChan() <-chan []byte
+	// Stop stops the transport. This will also close the channel returned by
+	// Done method.
+	Stop()
+	// Done returns a channel which will close when calling the Stop() method.
+	Done() chan struct{}
+}
+
+type TransportErr struct {
+	Message []byte
+	Name    string
+	Err     error
+}
+
+func (te *TransportErr) Error() string {
+	if len(te.Message) > 0 {
+		return fmt.Sprintf("%s: |%s| <- `%s`", te.Err, te.Name, te.Message)
+	}
+	return fmt.Sprintf("%s: |%s|", te.Err, te.Name)
+}
